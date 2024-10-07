45dc9303dcb000196852fa69bd4984f31ec2cd6a
FranMack <franmack556@gmail.com>
2024-10-07 17:10:51 -0300

    FEAT: lazy loading

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -1,5 +1,5 @@
-//https://nutriendolavida-api-91d5a690b9b6.herokuapp.com
+
 
 export const envs={
-    API_DOMAIN:"http://localhost:3000"
+    API_DOMAIN:"https://nutriendolavida-api-91d5a690b9b6.herokuapp.com"
 }
